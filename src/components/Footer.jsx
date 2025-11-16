d4f24f9c2a079deb07a9fc531854d9a6dfeefe21
andbxnk <tanabut1955@gmail.com>
2025-11-16 10:32:20 +0700

    v2.2

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <>
       <h1 className="text-center text-sm my-10">
-        Created by NinniN DTI SAU
+        Created by andbxnk DTI SAU
         <br />
         Copyright © 2025 Southeast Asia University
       </h1>
